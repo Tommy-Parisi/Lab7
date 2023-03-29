035fe1e17cbcc7375f5253ddc9e33634905af8df
joeydare <joeydare@comcast.net>
2023-03-29 16:58:04 -0400

    Version 1.0

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,19 +1,34 @@
+/**
+ * This class creates Team
+ * @Author Joey Dare & Tommy Parisi
+ * @Version 1.0
+ */
+
 public class Team {
     private String color;
     private ArrayList<Unit> teamUnit;
+    /**
+     * This constructor creates a team and assigns the color and Unit Team ArrayList to its properties
+     * @param color assigns the color passed to the property value
+     * @param team assigns the ArrayList of Units passed to the property value
+     */
     public Team(String color, ArrayList<Unit> team) {
         this.color = color;
         this.teamUnit = team;
     }
+    //Returns the team color
     public String getTeamColor() {
         return this.color;
     }
+    //Returns the ArrayList of Units
     public ArrayList<Unit> getTeamUnits() {
         return this.teamUnit;
     }
+    //This mutator method removes the unit passed into the function from the ArrayList of Units
     public void removeUnitsFromTeam(Unit unit) {
         this.teamUnit.remove(unit);
     }
+    //This mutator method adds the unit passed into the function from the ArrayList of Units
     public void addUnitsToTeam(Unit unit) {
         unit.setColor(this.color);
         this.teamUnit.add(unit);
