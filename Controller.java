99a39690698392655b2b1825abbcac3fe6aa2aca
ljaffee <128612976+ljaffee@users.noreply.github.com>
2023-05-16 16:42:46 -0400

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Controller {
     private Game game;
     private TextView text;
+    private GameEventLinkedList linkedList;
 
     public Game setUpGameModel(){
         // Create 4 pieces for team A
@@ -11,18 +13,20 @@ public class Controller {
 
         BartSimpsonUnit bs = new BartSimpsonUnit();
         bs.setTeamColor("Blu");
-        BartSimpsonUnit bart = new BartSimpsonUnit();
-        bart.setTeamColor("Blu");
 
         TomJerryUnit tj = new TomJerryUnit();
         tj.setTeamColor("Blu");
-        TomJerryUnit tom = new TomJerryUnit();
-        tom.setTeamColor("Blu");
+
+        StewieGriffinUnit g = new StewieGriffinUnit();
+        g.setTeamColor("Blu");
+
+        SquidwardUnit s = new SquidwardUnit();
+        s.setTeamColor("Blu");
 
         piecesTeamA.add(bs);
         piecesTeamA.add(tj);
-        piecesTeamA.add(bart);
-        piecesTeamA.add(tom);
+        piecesTeamA.add(g);
+        piecesTeamA.add(s);
 
         // Create a team object
         Team teamA = new Team("Blu",piecesTeamA);
@@ -34,18 +38,20 @@ public class Controller {
 
         BartSimpsonUnit bs2 = new BartSimpsonUnit();
         bs2.setTeamColor("Red");
-        BartSimpsonUnit bart2 = new BartSimpsonUnit();
-        bart2.setTeamColor("Red");
 
         TomJerryUnit tj2 = new TomJerryUnit();
         tj2.setTeamColor("Red");
-        TomJerryUnit tom2 = new TomJerryUnit();
-        tom2.setTeamColor("Red");
+
+        StewieGriffinUnit g2 = new StewieGriffinUnit();
+        g2.setTeamColor("Red");
+
+        SquidwardUnit s2 = new SquidwardUnit();
+        s2.setTeamColor("Red");
 
         piecesTeamB.add(bs2);
         piecesTeamB.add(tj2);
-        piecesTeamB.add(bart2);
-        piecesTeamB.add(tom2);
+        piecesTeamB.add(g2);
+        piecesTeamB.add(s2);
 
         // Create a team object
         Team teamB = new Team("Red",piecesTeamB);
@@ -59,42 +65,67 @@ public class Controller {
         this.game = setUpGameModel();
         this.text = new TextView();
         this.text.updateView(this.game);
+        this.linkedList = new GameEventLinkedList();
     }
 
     public void carryOutAction(int fromRow, int fromCol, int toRow, int toCol, char action) {
+        String actionString = "";
+        String eventType = "";
         if (action == 'M') {
-            ActionMove move = new ActionMove(this.game, fromRow, fromCol, toRow, toCol);
+            ActionMove move = new ActionMove(fromRow, fromCol, toRow, toCol, this.game);
             move.performAction();
+            actionString = move.toString();
+            eventType = "M";
         }
         if (action == 'A') {
-            ActionAttack attack = new ActionAttack(this.game, fromRow, fromCol, toRow, toCol);
+            ActionAttack attack = new ActionAttack(fromRow, fromCol, toRow, toCol, this.game);
             attack.performAction();
+            actionString = attack.toString();
+            eventType = "A";
         }
         if (action == 'R') {
-            ActionRecruit recruit = new ActionRecruit(this.game, fromRow, fromCol, toRow, toCol);
+            ActionRecruit recruit = new ActionRecruit(fromRow, fromCol, toRow, toCol, this.game);
             recruit.performAction();
+            actionString = recruit.toString();
+            eventType = "R";
         }
         if (action == 'S') {
-            ActionSpawn spawn = new ActionSpawn(this.game, fromRow, fromCol, toRow, toCol);
+            ActionSpawn spawn = new ActionSpawn(fromRow, fromCol, toRow, toCol, this.game);
             spawn.performAction();
+            actionString = spawn.toString();
+            eventType = "S";
         }
+        GameEvent event = new GameEvent(this.game.getCurrentPlayer().getPlayerNumber(), eventType, actionString);
+        GameEventNode eventNode = new GameEventNode(event);
+        this.linkedList.push(eventNode);
     }
-    public void playGame(){
-        while (!(this.game.isGameEnded())){
-            this.text.getNextPlayersAction(this.game);
-            while (!(Rules.checkValidAction(this.game, this.text.getFromSquareRow(), this.text.getFromSquareCol(), this.text.getToSquareRow(), this.text.getToSquareCol(), this.text.getActionType()))){
+
+    public void playGame() {
+        while (!(this.game.isGameEnded())) {
+            text.getNextPlayersAction(this.game);
+            while (!(Rules.checkValidAction(this.game, text.getFromSquareRow(), text.getFromSquareCol(), text.getToSquareRow(), text.getToSquareCol(), text.getActionType()))) {
                 System.out.println("Not valid");
-                this.text.getNextPlayersAction(this.game);
+                text.getNextPlayersAction(this.game);
             }
-            carryOutAction(this.text.getFromSquareRow(), this.text.getFromSquareCol(), this.text.getToSquareRow(), this.text.getToSquareCol(), this.text.getActionType());
+            carryOutAction(text.getFromSquareRow(), text.getFromSquareCol(), text.getToSquareRow(), text.getToSquareCol(), text.getActionType());
             System.out.println(this.game);
         }
-        System.out.println("Game over: " + this.game.getWinner() + " wins");
+        // New addition
+        System.out.println("Winning move: " + this.linkedList.pop().getEventDetails());
+        ArrayList<GameEventLinkedList> gameEventLinkedLists = new ArrayList<>();
+        gameEventLinkedLists.add(this.linkedList.pop("M"));
+        gameEventLinkedLists.add(this.linkedList.pop("A"));
+        gameEventLinkedLists.add(this.linkedList.pop("R"));
+        gameEventLinkedLists.add(this.linkedList.pop("S"));
+        Collections.sort(gameEventLinkedLists);
+        for (GameEventLinkedList eventList : gameEventLinkedLists) {
+            System.out.println("Event Type: " + text.getActionType() + "Size: " + eventList.countEvents());
+        }
+        text.printEndOfGameMessage(this.game);
     }
+
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        controller.playGame();
+        Controller newController = new Controller();
+        newController.playGame();
     }
-
-
 }
---
+++ GameEventLinkedList.java
@@ -0,0 +1,88 @@
+public class GameEventLinkedList implements Comparable<GameEventLinkedList>{
+    private GameEventNode head;
+    private int size;
+
+    public GameEventLinkedList(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty(){
+        return this.head == null;
+    }
+
+    public void push(GameEventNode eventNode){
+        if (isEmpty()) {
+            this.head = eventNode;
+        }
+        else {
+            eventNode.setNext(this.head);
+            this.head = eventNode;
+            this.size++;
+        }
+    }
+
+    public GameEvent pop() {
+        GameEventNode temp = this.head;
+        this.head = this.head.getNext();
+        temp.setNext(null);
+        this.size--;
+        return temp.getGameState();
+    }
+
+    public GameEventLinkedList pop(String eventType){
+        GameEventNode current = head;
+        GameEventNode lastNode = null;
+        GameEventLinkedList list = new GameEventLinkedList();
+        while (current.getNext() != null) {
+            if (current.getGameState().getEventType().equals(eventType)) {
+                if (lastNode == null) {
+                    this.head = current.getNext();
+                }
+                else {
+                    lastNode.setNext(current.getNext());
+                }
+                current.setNext(null);
+                list.push(current);
+                this.size--;
+                if (lastNode == null) {
+                    current = this.head;
+                }
+                else {
+                    current = lastNode.getNext();
+                }
+            }
+            else {
+                lastNode = current;
+                current = current.getNext();
+            }
+        }
+        return list;
+    }
+
+    public void display() {
+        GameEventNode current = head;
+        while (current != null) {
+            System.out.println(current.getGameState().getEventDetails());
+            current = current.getNext();
+        }
+    }
+
+    // New modification
+    public int countEvents(){
+        int count = 0;
+        GameEventNode current = head;
+        while (current != null) {
+            count += 1;
+            current = current.getNext();
+        }
+        return count;
+    }
+
+
+    @Override
+    public int compareTo(GameEventLinkedList otherList){
+        return Integer.valueOf(this.size).compareTo(otherList.size);
+    }
+
+}
