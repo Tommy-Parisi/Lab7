74f882954429ec1ee37c5dc274f7371e119cb792
ljaffee <128612976+ljaffee@users.noreply.github.com>
2023-05-18 17:09:10 -0400

    Update Recruiter.java

--- src/main/java/Recruiter.java
+++ src/main/java/Recruiter.java
@@ -1,11 +1,12 @@
 public abstract class Recruiter extends Unit {
     private int numRecruits;
 
+    // Updated to include points
     public Recruiter(char symbol, String name, double health, double healthModifier,
                      double damage, double damageModifier, int luck, int xCor, int yCor,
-                     int movement, int movementModifier, String teamColor, int numRecruits){
+                     int movement, int movementModifier, String teamColor, int numRecruits, int points){
         super(symbol, name, health, healthModifier, damage, damageModifier,
-                luck, xCor, yCor, movement, movementModifier, teamColor);
+                luck, xCor, yCor, movement, movementModifier, teamColor, points);
         this.numRecruits = numRecruits;
     }
     public int getNumRecruits() {
