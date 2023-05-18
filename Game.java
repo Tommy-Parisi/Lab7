f8e0c13e576a0ddb335c468c447b1ebbd64efffa
Tommy-Parisi <128612741+Tommy-Parisi@users.noreply.github.com>
2023-05-18 14:30:10 -0400

    Add files via upload
    added functionality for eject special square

--- Game.java
+++ Game.java
@@ -1,77 +1,92 @@
-import java.util.ArrayList;
 import java.util.Collections;
 
-public class Game {
-    private GameBoard board;
+/**
+ * This class creates a Game
+ * @Author Tommy Parisi & Joey Dare
+ * @Version 1.0
+ */
+public class Game{
+    private GameBoard gameBoard;
     private Player player1;
     private Player player2;
 
-    private void initializeGameBoard(int numRows, int numCols) {
-        board = new GameBoard(numRows, numCols);
-        ArrayList<Unit> teamUnits = Team.getTeamUnits();
-
-        for (int i = 0; i < teamUnits.size(); i++) {
-            GameBoard testGameBoard = new GameBoard(numRows, numCols);
-            BoardSquare emptySpace = testGameBoard.findRandomEmptySpace();
-            emptySpace.setUnit(teamUnits.get(i));
-
-        }
-    }
-
-    public Game(int numRows, int numCols, Player player1, Player player2) {
+    public Game(int numRows, int numColumns, Player player1, Player player2){
         this.player1 = player1;
         this.player2 = player2;
-        initializeGameBoard(numRows, numCols);
+        initializeGameBoard(numRows, numColumns);
+
+
+    }
+    private GameBoard initializeGameBoard(int numRows, int numColumns){
+        this.gameBoard = new GameBoard(numRows, numColumns);
+        //insert code for the following algorithm:
+        /**
+         * Create an instance of GameBoard with the specified number of rows and columns.
+         * For each unit in both player's teams:
+         * a. Use the findRandomEmptySpace method in the GameBoard class to get a random empty space on the board.
+         * b. Set the unit's coordinates to the coordinates of the empty space.
+         * c. Place the unit on the board at the chosen coordinates.
+         * Return the completed GameBoard.
+         */
+        for (Unit unit : player1.getTeam().getTeamUnits()) {
+            BoardSquare randomSquare = gameBoard.findRandomEmptySpace();
+            randomSquare.setUnit(unit);
+        }
+        for (Unit unit : player2.getTeam().getTeamUnits()) {
+            BoardSquare randomSquare = gameBoard.findRandomEmptySpace();
+            randomSquare.setUnit(unit);
+        }
+
+        BoardSquare specialSquare = gameBoard.findRandomEmptySpace(); //initializes special square
+        specialSquare.setSpecial(specialSquare); //makes specialSquare special
+
+        return gameBoard;
     }
 
     public GameBoard getGameBoard() {
-        return board;
+        return gameBoard;
     }
-
     public Player getCurrentPlayer() {
-        if (player1.isTurn()) {
+        if(player1.isTurn()){
             return player1;
         }
-        else {
+        else{
             return player2;
         }
     }
-
     public Player getOpponentPlayer() {
-        if (player1.isTurn()) {
+        if(player1.isTurn()){
             return player2;
         }
-        else {
+        else{
             return player1;
         }
     }
 
     public boolean isTurn(Player player) {
-        if (player.isTurn()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return player.isTurn();
     }
-
     public BoardSquare[][] getBoardSquares() {
-        return this.getGameBoard().getSquares();
+        return getGameBoard().getSquares();
     }
 
+    /**
+     * This mutator method changes both players turn status
+     * from its current status to the opposite
+     */
     public void changeTurn() {
-        player1.setTurn(player1.isTurn());
-        player2.setTurn(player2.isTurn());
+        player1.setTurn(!isTurn(player1));
+        player2.setTurn(!isTurn(player2));
     }
 
     public boolean isAWinner(){
-        return (player1.getPlayersTeam().getTeamUnits().size() == 0) ^ (player2.getPlayersTeam().getTeamUnits().size() == 0);
+        return (player1.getTeam().getTeamUnits().size() == 0) ^ (player2.getTeam().getTeamUnits().size() == 0);
     }
 
     public Player getWinner() {
-        if (player1.getPlayersTeam().getTeamUnits().size() == 0) {
+        if (player1.getTeam().getTeamUnits().size() == 0) {
             return player2;
-        } else if (player2.getPlayersTeam().getTeamUnits().size() == 0) {
+        } else if (player2.getTeam().getTeamUnits().size() == 0) {
             return player1;
         } else {
             return null;
@@ -79,22 +94,21 @@ public class Game {
     }
 
     public boolean isGameEnded(){
-        return (player1.getPlayersTeam().getTeamUnits().size() == 0) || (player2.getPlayersTeam().getTeamUnits().size() == 0);
+        return (player1.getTeam().getTeamUnits().size() == 0) || (player2.getTeam().getTeamUnits().size() == 0);
     }
 
     public String toString(){
         StringBuilder retString = new StringBuilder();
         retString.append("Game Board:\n")
-                .append(String.join("", Collections.nCopies(10 + board.getNumColumns()*8, "*")))
-                .append("\n" + getBoardSquares().toString())
-                .append(String.join("", Collections.nCopies(10 + board.getNumColumns()*8, "*")))
-                .append("\n" + getCurrentPlayer().getPlayersTeam().toString() + "\n")
-                .append(String.join("", Collections.nCopies(10 + board.getNumColumns()*8, "*")))
-                .append("\n" + getOpponentPlayer().getPlayersTeam().toString() + "\n")
-                .append(String.join("", Collections.nCopies(10 + board.getNumColumns()*8, "*")))
-                .append("\nIt is Player " + getCurrentPlayer().getPlayerNumber() + "'s (" + getCurrentPlayer().getPlayersTeam().getTeamColor() + "'s) turn\n");
+                .append(String.join("", Collections.nCopies(10 + gameBoard.getNumColumns()*8, "*")))
+                .append("\n" + getGameBoard().toString())
+                .append(String.join("", Collections.nCopies(10 + gameBoard.getNumColumns()*8, "*")))
+                .append("\n" + getCurrentPlayer().getTeam().toString() + "\n")
+                .append(String.join("", Collections.nCopies(10 + gameBoard.getNumColumns()*8, "*")))
+                .append("\n" + getOpponentPlayer().getTeam().toString() + "\n")
+                .append(String.join("", Collections.nCopies(10 + gameBoard.getNumColumns()*8, "*")))
+                .append("\nIt is Player " + getCurrentPlayer().getPlayerNumber() + "'s (" + getCurrentPlayer().getTeam().getTeamColor() + "'s) turn\n");
         return retString.toString();
     }
 
 }
-
